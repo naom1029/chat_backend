7e90474eb5bcb4482b13002efcc691bb1548a9c2
naom971029 <bluesky.97102945@gmail.com>
2024-09-23 15:13:37 +0900

    ADD: メッセージモデルにコメント追加

--- src/models/message.rs
+++ src/models/message.rs
@@ -2,11 +2,14 @@ use actix::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+// クライアントから送信され送信されるメっセージ
 #[derive(Clone, Message, Serialize, Deserialize)]
 #[rtype(result = "()")]
 pub struct ClientMessage {
     pub text: String,
 }
+
+// チャットメッセージ
 #[derive(Clone, Message, Serialize, Deserialize)]
 #[rtype(result = "()")]
 pub struct ChatMessage {
@@ -14,11 +17,15 @@ pub struct ChatMessage {
     pub text: String,
     pub timestamp: String,
 }
+
+// システムメッセージ
 #[derive(Clone, Message, Serialize, Deserialize)]
 #[rtype(result = "()")]
 pub struct SystemMessage {
     pub text: String,
 }
+
+// サーバーに参加するためのメッセージ
 #[derive(Clone, Message)]
 #[rtype(result = "Uuid")]
 pub struct JoinServer {
@@ -26,16 +33,21 @@ pub struct JoinServer {
     pub client_name: Option<String>,
     pub client: Recipient<ChatMessage>,
 }
+
+// サーバーから退出するためのメッセージ
 #[derive(Clone, Message, Serialize, Deserialize)]
 #[rtype(result = "()")]
 pub struct LeaveServer {
     pub server_name: String,
     pub client_id: Uuid,
 }
+
+// サーバーのリストを取得するためのメッセージ
 #[derive(Clone, Message, Serialize, Deserialize)]
 #[rtype(result = "Vec<String>")]
 pub struct ListServer;
 
+// サーバーを選択するためのメッセージ
 pub struct SelectServer {
     pub server_name: String,
     pub client_id: Uuid,
